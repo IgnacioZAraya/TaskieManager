0ddd95a6719902ea86d51df910db2ae28a0fe7d4
Esteban Alonso Barrera Araya <estebanbarrera104@gmail.com>
2024-07-29 15:38:32 -0600

    Commit TaskieSchedule

---
+++ demo/src/main/java/com/project/demo/logic/entity/taskie/TaskieSchedule.java
@@ -0,0 +1,47 @@
+package com.project.demo.logic.entity.taskie;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Component
+public class TaskieSchedule {
+    @Autowired
+    private TaskieRepository taskieRepository;
+
+    @Scheduled(fixedRate = 1000)
+    @Transactional
+    public void reduceTaskieStats() {
+        List<Taskie> taskies = taskieRepository.findAll();
+        for (Taskie taskie : taskies) {
+            int reduceLifeBy = 0;
+
+            if (taskie.getCleanse() > 0) {
+                taskie.setCleanse(Math.max(taskie.getCleanse() - 1, 0));
+            } else {
+                reduceLifeBy++;
+            }
+
+            if (taskie.getHunger() > 0) {
+                taskie.setHunger(Math.max(taskie.getHunger() - 1, 0));
+            } else {
+                reduceLifeBy++;
+            }
+
+            if (taskie.getEnergy() > 0) {
+                taskie.setEnergy(Math.max(taskie.getEnergy() - 1, 0));
+            } else {
+                reduceLifeBy++;
+            }
+
+            if (reduceLifeBy > 0 && taskie.getLife() > 0) {
+                taskie.setLife(Math.max(taskie.getLife() - reduceLifeBy, 0));
+            }
+
+            taskieRepository.save(taskie);
+        }
+    }
+}
